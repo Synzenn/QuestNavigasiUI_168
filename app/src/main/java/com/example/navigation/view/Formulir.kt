3738283d5541e98538073061f009cb4f1234f652
Synzenn <velriel42@gmail.com>
2025-11-07 13:40:54 +0700

    membuat row untuk jenis kelamin, dan mengatur tata letaknya

--- app/src/main/java/com/example/navigation/view/Formulir.kt
+++ app/src/main/java/com/example/navigation/view/Formulir.kt
@@ -55,6 +55,21 @@ fun FormIsian(
                 label = {Text(text = "Nama Lengkap")},
                 onValueChange = {},
             )
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness,color = Color.Red)
+            Row{
+                jenisK.forEach {
+                    item ->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
 
 
 }
