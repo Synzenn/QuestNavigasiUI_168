afd5fd3080978520ea2c3d851ac01d3e3db1396d
Synzenn <velriel42@gmail.com>
2025-11-07 14:28:29 +0700

    membuat TampilData dan memodifikasinya

--- app/src/main/java/com/example/navigation/view/TampilData.kt
+++ app/src/main/java/com/example/navigation/view/TampilData.kt
@@ -25,3 +25,66 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navigation.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()->Unit
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
+        Pair(stringResource(id = R.string.alamat),"Yogyakarta"),
+    )
+
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id= R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(
+                        colorResource(id= R.color.teal_700)
+                    )
+            )
+        }
+    ){ isiRuang ->
+
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.spacedBy(
+                dimensionResource(id = R.dimen.padding_small)
+            )
+        ) {
+            items.forEach { item ->
+                Column {
+                    Text(
+                        text = item.first.uppercase(),
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = item.second,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Cursive,
+                        fontSize = 22.sp
+                    )
+                }
+                HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick
+            ){
+                Text(text = stringResource(id= R.string.back))
+            }
+        }
+    }
+}
